a8d165a9e2b41cad416afa7bd56ce21124fe7ae9
Rui Oliveira <ruimail24@gmail.com>
2023-11-09 13:57:51 +0000

    Change default visibility

---
+++ cmake/default_visibility.cmake
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Gregorian Calendar [Static] HTML [Page] Generator
+#
+# SPDX-FileCopyrightText: 2022 Rui Oliveira <ruimail24@gmail.com>
+
+include_guard(GLOBAL)
+
+set(CMAKE_C_VISIBILITY_PRESET "hidden")
+set(CMAKE_CXX_VISIBILITY_PRESET "hidden")
+set(CMAKE_VISIBILITY_INLINES_HIDDEN ON)

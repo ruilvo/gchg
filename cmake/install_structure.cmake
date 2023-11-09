16dedd712c98d10b812861e878b863d78679a2a6
Rui Oliveira <ruimail24@gmail.com>
2023-11-09 13:58:30 +0000

    Set the install structure

---
+++ cmake/install_structure.cmake
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Gregorian Calendar [Static] HTML [Page] Generator
+#
+# SPDX-FileCopyrightText: 2022 Rui Oliveira <ruimail24@gmail.com>
+
+include_guard(GLOBAL)
+
+include(GNUInstallDirs)
+
+# Professional CMake 27.1.1
+set(CMAKE_INSTALL_DOCDIR ${CMAKE_INSTALL_DATAROOTDIR}/doc/${PROJECT_NAME})

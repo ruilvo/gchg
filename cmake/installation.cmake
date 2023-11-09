ab7defb2afbcab00c753dbad1340be34fa9275c4
Rui Oliveira <ruimail24@gmail.com>
2023-11-09 13:58:53 +0000

    Add basic install code

---
+++ cmake/installation.cmake
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Gregorian Calendar [Static] HTML [Page] Generator
+#
+# SPDX-FileCopyrightText: 2022 Rui Oliveira <ruimail24@gmail.com>
+
+include_guard(GLOBAL)
+
+include(CMakePackageConfigHelpers)
+
+# Install gchg_app
+install(TARGETS gchg_app)
